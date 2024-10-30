ea4b3a442938bf9697b38c4d7ea1d73106ef964b
LautaroZalazar <lautyzalazar10@gmail.com>
2024-10-30 16:53:58 -0300

    feat: :art: Project initialize
    Project initialize

---
+++ src/main.ts
@@ -0,0 +1,40 @@
+import 'dotenv/config';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { json, urlencoded } from 'express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import config from './config';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.use(json({ limit: '5mb' }));
+  app.use(urlencoded({ extended: true, limit: '5mb' }));
+  app.useGlobalPipes(new ValidationPipe());
+  app.setGlobalPrefix(process.env.APP_GLOBAL_PREFIX);
+  const documentationConfig = new DocumentBuilder()
+    .setTitle('MovingRentals')
+    .setDescription('')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, documentationConfig);
+  SwaggerModule.setup('api/documentation', app, document);
+  await app.listen(config().app.app_port || 3000);
+}
+bootstrap()
+  .then(() => {
+    console.log(
+      'Listening on: http://localhost:' +
+      config().app.app_port +
+      '/' +
+      config().app.app_global_prefix,
+    );
+    console.log('Server started successfully 🎸 ');
+  })
+  .catch((e) => {
+    console.log('Server failed to start');
+    console.log(e);
+  });
