7f571dcdf3a3fea2fac32999c324bd92385a159c
LautaroZalazar <lautyzalazar10@gmail.com>
2025-02-15 00:53:17 -0300

    add reservations to vehicle

--- src/vehicle/domain/repositories/vehicle.interface.repository.ts
+++ src/vehicle/domain/repositories/vehicle.interface.repository.ts
@@ -4,6 +4,7 @@ import { VehicleModel } from "../models/vehicle.model";
 export interface IVehicleRepository {
     create(vehicle: VehicleModel): Promise<VehicleModel>;
     findById(id: string): Promise<VehicleModel>;
+    findByDate(query: any): Promise<VehicleModel[]>;
     findAll(): Promise<VehicleModel[]>;
     update(id: string, vehicle: VehicleModel): Promise<VehicleModel>
     updatePriceByModel(model: string, prices: UpdatePriceByModelDTO): Promise<void>
